3ed8782abaef5f55ed85dfd1bccdee701db9103e
Fernando Artur Luithardt <91349414+Fernando-Artur-Luithardt@users.noreply.github.com>
2023-03-01 00:12:27 -0300

    Front copm jquery, funcionando básico: dra n drop e salvando no banco

---
+++ back-end/prisma/migrations/20230301020200_add_order_card/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_card" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "tarefa" TEXT NOT NULL,
+    "colunaId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "order" INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT "card_colunaId_fkey" FOREIGN KEY ("colunaId") REFERENCES "colunas" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_card" ("colunaId", "createdAt", "id", "tarefa") SELECT "colunaId", "createdAt", "id", "tarefa" FROM "card";
+DROP TABLE "card";
+ALTER TABLE "new_card" RENAME TO "card";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
